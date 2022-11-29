10cf3413d849fc8740cc2fcf8fd81e61a5b48621
vic900511 <a0966176196@gmail.com>
2022-11-29 22:18:31 +0800

    [add] Add the file

--- flyming/flyming/urls.py
+++ flyming/flyming/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Web.views import contact
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("contact/", contact),
 ]
