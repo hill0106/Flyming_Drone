c99bab440cc9f0c2c9e22ea8b36d7b27758f1874
hill0106 <hill0106trinity260935@gmail.com>
2022-12-04 00:34:25 +0800

    [add] Add projectPage.

--- flyming/static/app.js
+++ flyming/static/app.js
@@ -7,3 +7,36 @@ window.addEventListener("scroll", e => {
     else
         header.style = "";
 })
+
+$(document).ready(function(){
+    $("#carousel1").owlCarousel({
+        loop: true, // 循環播放
+        margin: 30, // 外距 10px
+        responsiveClass:true,
+        responsive: {
+          0: {
+            items: 1,
+            nav: true
+          },
+          600: {
+            items: 3, // 螢幕大小為 600~1000 顯示 3 個項目
+            nav: true
+          },
+          
+        }
+      });
+
+      $("#carousel2").owlCarousel({
+        loop: true, // 循環播放
+        margin: 30, // 外距 10px
+        responsiveClass:true,
+        responsive: {
+          0: {
+            items: 1,
+            nav: true,
+          },
+        }
+      });
+
+    $(`.owl-nav`).css('display', 'flex');
+});
