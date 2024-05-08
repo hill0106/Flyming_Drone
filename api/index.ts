8e411e0fe6fd9647eb317858e8ab3feadd6bb396
hill0106 <hill0106trinity260935@gmail.com>
2024-05-08 13:46:15 +0800

    deploy to vercel

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+// const ejs = require("ejs");
 
 // youtube API
 const youtube = require("../youtube");
